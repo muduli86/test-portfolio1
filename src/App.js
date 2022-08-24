1d7e82e61c25236de82d4cb1774aff1f973d8402
Simanta muduli <muduli.simanta@gmail.com>
2022-08-24 08:43:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { loadFull } from "tsparticles";
 
 import { About, Home, Contact, Portfolio, Resume, Skills } from "./pages";
 import Navbar from "./components/navbar";
+import particlesConfig from "./helpers/particlesConfig";
 
 function App() {
   const particlesinit = async (main) => {
@@ -11,7 +12,11 @@ function App() {
   };
   return (
     <div>
-      <Particles id='particles' options={} init={particlesinit} />
+      <Particles
+        id='particles'
+        options={particlesConfig}
+        init={particlesinit}
+      />
       <Navbar />
       <Routes>
         <Route path='/' index element={<Home />} />
