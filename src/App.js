0a5066fd43f2b8924e75e475e83b350e1683c8f3
Simanta muduli <muduli.simanta@gmail.com>
2022-08-24 08:32:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 import { About, Home, Contact, Portfolio, Resume, Skills } from "./pages";
 import Navbar from "./components/navbar";
 
 function App() {
+  const particlesinit = async (main) => {
+    await loadFull(main);
+  };
   return (
     <div>
+      <Particles id='particles' options={} init={particlesinit} />
       <Navbar />
       <Routes>
         <Route path='/' index element={<Home />} />
